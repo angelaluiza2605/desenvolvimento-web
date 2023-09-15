98e0c138a0ca5f9b1324aa0afd101126b029411d
Luiza Souza <angelaluizapereira679@gmail.com>
2023-09-15 15:34:38 -0300

    add project

---
+++ 4-JS/Class-01-data-types/Class-02operators/desafio.js
@@ -0,0 +1,12 @@
+let yourName = prompt("digite seu nome");
+console.log(yourName);
+alert(" Bem vindo(a) " +  yourName  + " vamos fazer uma soma ");
+let numero1 = parseInt(prompt("digite o primeiro numero"))
+let numero2 = parseInt(prompt("digite o segundo numero"))
+console.log (parseInt ( numero1 ));
+console.log (parseInt ( numero2));
+
+let soma = numero1 + numero2
+console.log(soma);
+
+alert("A soma do  " + numero1 + numero2 + " é " + soma);
---
+++ 4-JS/Class-01-data-types/script.js
@@ -0,0 +1,31 @@
+console.log("Hello,word!");
+
+let student = "Luiza"
+let age= "19"
+let height= "158"
+console.log (typeof student);
+console.log(typeof age);
+console.log(23);
+console.log(height);
+
+let num1= 2;
+let num2= 8;
+
+console.log(num1 + num2);
+
+let isStudentInCvt = true;
+
+console.log(isStudentInCvt);
+console.log( typeof isStudentInCvt
+    );
+
+    console.log(10 == 88);
+    console.log(10 == 88);
+    console.log(10 > 9);
+    console.log(10 < 1);
+    console.log(10 > 67);
+    console.log(10 != 3);
+
+    let yourName = prompt("digite seu nome");
+
+    console.log(yourName);
