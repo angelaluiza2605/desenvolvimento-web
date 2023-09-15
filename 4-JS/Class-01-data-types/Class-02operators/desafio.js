8d1f7e441234aded1bc4eb6ae6818969818a3101
Luiza Souza <angelaluizapereira679@gmail.com>
2023-09-15 19:00:22 -0300

    add update

--- 4-JS/Class-01-data-types/Class-02operators/desafio.js
+++ 4-JS/Class-01-data-types/Class-02operators/desafio.js
@@ -1,8 +1,8 @@
-let yourName = prompt("digite seu nome");
+let yourName = prompt("Digite seu nome");
 alert(" Bem vindo(a) " +  yourName  + " vamos fazer uma soma ");
 
-let numero1 = parseInt(prompt("digite o primeiro numero"));
-let numero2 = parseInt(prompt("digite o segundo numero"));
+let numero1 = parseInt(prompt("Digite o primeiro numero"));
+let numero2 = parseInt(prompt("Digite o segundo numero"));
 let soma = numero1 + numero2;
 
 alert("A soma do " + numero1 + " junto com o " + numero2 + " é " + soma);
