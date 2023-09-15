eeaa416f986f104a0e3c03c0981ffac24922ff9c
Luiza Souza <angelaluizapereira679@gmail.com>
2023-09-15 18:37:22 -0300

    alter resolution challenge datatypes

--- 4-JS/Class-01-data-types/Class-02operators/desafio.js
+++ 4-JS/Class-01-data-types/Class-02operators/desafio.js
@@ -1,12 +1,8 @@
 let yourName = prompt("digite seu nome");
-console.log(yourName);
 alert(" Bem vindo(a) " +  yourName  + " vamos fazer uma soma ");
-let numero1 = parseInt(prompt("digite o primeiro numero"))
-let numero2 = parseInt(prompt("digite o segundo numero"))
-console.log (parseInt ( numero1 ));
-console.log (parseInt ( numero2));
 
-let soma = numero1 + numero2
-console.log(soma);
+let numero1 = parseInt(prompt("digite o primeiro numero"));
+let numero2 = parseInt(prompt("digite o segundo numero"));
+let soma = numero1 + numero2;
 
 alert("A soma do " + numero1 + " junto com o " + numero2 + " é " + soma);
